0dbd048d33cef8f4ecaf0409391db7f156994d8f
Lefteris <etroullouris@gmail.com>
2022-11-20 12:10:00 -0500

    final project lab 2

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,18 +1,27 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+from nested_admin import NestedTabularInline, NestedModelAdmin
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(NestedTabularInline):
+    model=Choice
+    extra=4
+
+class QuestionInline(NestedTabularInline):
+    model=Question
+    inlines = [ChoiceInline]
+    extra=5
 
 
-class LessonInline(admin.StackedInline):
+class LessonInline(NestedTabularInline):
     model = Lesson
     extra = 5
 
 
 # Register your models here.
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+class CourseAdmin(NestedModelAdmin):
+    inlines = [LessonInline, QuestionInline,]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -21,6 +30,9 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(NestedModelAdmin):
+    inlines = [ChoiceInline]
+
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +40,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
